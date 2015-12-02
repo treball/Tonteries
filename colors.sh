c5c70864988575bf11e90fe5d25d6523e56b558b
CesarBiker <CesarBiker@users.noreply.github.com>
2015-12-02 17:13:17 +0100

    Bash-Colors
    Un script senzill que va canviant els colors de la pantalla

--- colors.sh
+++ colors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Fitxer:		laal.sh
-# Autor:		iam39418281
+# Fitxer:		colors.sh
+# Autor:		Àngel Mariages
 # Data:			01/12/2015 18:13:33
 # Versió:		0.1
-# Llicència:	This is free software, licensed under the GNU General Public License v3.
-#				See http://www.gnu.org/licenses/gpl.html for more information.
-# Descripció:	Jeje tot guai i xulo!
+# Llicència:		This is free software, licensed under the GNU General Public License v3.
+#			See http://www.gnu.org/licenses/gpl.html for more information.
+# Descripció:		Jeje tot guai i xulo!
 
 c=("\e[48;5;9m" "\e[48;5;10m" "\e[48;5;11m" "\e[48;5;13m" "\e[48;5;14m" "\e[48;5;15m")
 h=$(tput lines)*100
